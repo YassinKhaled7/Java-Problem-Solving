994962c1923baf0c1366b278e76ab34972c7b4f4
Yassin Khaled Gaber <yassienkhaled274@gmail.com>
2025-03-11 17:59:39 +0200

    updated

--- FirstNonRepeatingQueue.java
+++ FirstNonRepeatingQueue.java
@@ -1,27 +1,29 @@
 import java.util.*;
 
-public class FirstNonRepeatingQueue {
-    public static int firstNonRepeating(Queue<Integer> queue) {
+public class FirstNonRepeatingQueue 
+{
+    public static int firstNonRepeating(Queue<Integer> queue) 
+    {
         Map<Integer, Integer> freqMap = new HashMap<>();
-
-        // Count occurrences of each element
-        for (int num : queue) {
+        for (int num : queue) 
+        {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-
-        // Find the first non-repeating element
-        for (int num : queue) {
-            if (freqMap.get(num) == 1) return num;
+        for (int num : queue) 
+        {
+            if (freqMap.get(num) == 1)
+                return num;
         }
 
-        return -1; // If all elements repeat
+        return -1;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Queue<Integer> queue = new LinkedList<>(Arrays.asList(4, 5, 1, 2, 5, 1, 4));
-        System.out.println("First Non-Repeating: " + firstNonRepeating(queue)); // Output: 2
+        System.out.println("First Non-Repeating: " + firstNonRepeating(queue));
 
         Queue<Integer> queue2 = new LinkedList<>(Arrays.asList(7, 7, 7));
-        System.out.println("First Non-Repeating: " + firstNonRepeating(queue2)); // Output: -1
+        System.out.println("First Non-Repeating: " + firstNonRepeating(queue2));
     }
 }
